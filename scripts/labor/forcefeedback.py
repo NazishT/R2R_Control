ae8748721c5b0b38bfeeaa88b3b1d352cbf9787f
Naxxish <naxisht@gmail.com>
2020-07-21 15:24:04 -0400

    Create forcefeedback.py

---
+++ scripts/labor/forcefeedback.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+
+def callback(msg):
+        move = Twist()
+
+	for i in [60,120]:
+            if min(msg.ranges)> 0.5 and min(msg.ranges)<1:
+               print min(msg.ranges)
+               move.linear.x=0.0
+               move.angular.z=0.5
+               pub1.publish(move)
+               
+
+rospy.init_node('force_feedback')
+sub=rospy.Subscriber('/h2/scan', LaserScan, callback)
+pub1= rospy.Publisher('/h2/joy_teleop/cmd_vel',Twist, queue_size=10)
+
+rate=rospy.Rate(10)
+
+while not rospy.is_shutdown():
+     	rate.sleep()
