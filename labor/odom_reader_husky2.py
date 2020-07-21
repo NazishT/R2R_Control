7aa16e2bf265aade441b655cd41e3701bd0917b1
Naxxish <naxisht@gmail.com>
2020-07-21 12:21:35 -0700

    Create odom_reader_husky2.py

---
+++ labor/odom_reader_husky2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+
+v_x = 0
+w= 0
+
+def callback(msg):
+ 	
+        vel_msg = Twist()
+        global v_x, w
+       
+        v_x = msg.linear.x
+        w = msg.angular.z
+
+
+        vel_msg.linear.x = v_x
+        vel_msg.angular.z = w
+        pub.publish(vel_msg)
+
+        print "------------------------------------------"
+        print "velocity linear x=" +str(v_x)
+        print "velocity angular z=" +str(w)
+
+
+if __name__=='__main__':
+        rospy.init_node('odom_reader_husky', anonymous=True)
+        sub = rospy.Subscriber('h2/joy_teleop/cmd_vel', Twist, callback)
+        pub = rospy.Publisher('/twist_marker_server/cmd_vel', Twist, queue_size=10)
+        rate=rospy.Rate(10)
+       
+        while not rospy.is_shutdown():
+           rate.sleep()
